7b4caf9e74ee666eba954f4db41784783524f855
jassycliq <josalgado12709@gmail.com>
2020-05-22 14:13:51 -0700

    Create new DI for PlayBow API

--- app/src/main/java/com/playbowdogs/neighbors_dogsitter_android/di/NetworkModule.kt
+++ app/src/main/java/com/playbowdogs/neighbors_dogsitter_android/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package com.playbowdogs.neighbors_dogsitter_android.di
 
 import com.playbowdogs.neighbors_dogsitter_android.BuildConfig
 import com.playbowdogs.neighbors_dogsitter_android.data.api.ApiService
+import com.playbowdogs.neighbors_dogsitter_android.data.api.PBApiService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
@@ -15,6 +16,10 @@ import java.util.concurrent.TimeUnit
 val networkModule = module {
     single(named("BASE_ANGEL_CAM_URL")) {
         "https://api.angelcam.com/v1/"
+    }
+
+    single(named("PLAYBOW_API_URL")) {
+        "https://playbowtech.com/api/"
     }
 
     single {
@@ -40,14 +45,25 @@ val networkModule = module {
             .build()
     }
 
-    single {
+    single(named("AngelCamAPI")) {
         Retrofit.Builder().baseUrl(get<String>(named("BASE_ANGEL_CAM_URL")))
             .addConverterFactory(MoshiConverterFactory.create(get()))
             .client(get())
             .build()
     }
 
+    single(named("PlayBowAPI")) {
+        Retrofit.Builder().baseUrl(get<String>(named("PLAYBOW_API_URL")))
+            .addConverterFactory(MoshiConverterFactory.create(get()))
+            .client(get())
+            .build()
+    }
+
     single {
-        get<Retrofit>().create(ApiService::class.java)
+        get<Retrofit>(named("AngelCamAPI")).create(ApiService::class.java)
+    }
+
+    single {
+        get<Retrofit>(named("PlayBowAPI")).create(PBApiService::class.java)
     }
 }
